44ccfa98f919ddc10dfee7becce98b4e896f88aa
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-07-09 16:03:31 +0200

    Initial Commit

---
+++ TestLocalData/TestLocalDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLocalDataApp.swift
+//  TestLocalData
+//
+//  Created by Michele Manniello on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestLocalDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
